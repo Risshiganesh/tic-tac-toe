bec128e2d687e1ae2cdfee3ae748eac4da8ea6ca
Risshiganesh Villalan <risshiganesh1997@gmail.com>
2023-07-05 21:32:40 +0800

    Connect DOM to game logic

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //This is basically object manipulation
 
+// Put inside a module and send it to
 let gridNumber = 3; //put this in a module later, this is player input
+
+
+
+// put this inside a module
+
+
+
+
 
 //factory function
 const gameBoard = (function(gridNumberPlaceholder){
@@ -82,339 +91,442 @@ function consoleDisplay (boardObj){
 
 consoleDisplay(gameBoard);
 
-// console.log(gameBoard);
 
 
-// module for DOM
-const displayController = (function(){
 
 
+
+
+
+// COMPLETE THE FLOW AND MODULARISE EVERYTHING
+
+
+const gameFlow = (function () {
+    
+    // first create players
+    let player1turn = true;
+
+    const players = function (name,marker){
+
+        // may have to modify this to add marker without row and col
+        // const choice = function(row,col){
+    
+        //     console.log(name+" plays")
+    
+        //     let chosenGrid = '';
+        
+        //     gameBoard.board.forEach(grid => {
+    
+        //         // Only add the marker to the grid that was chosen
+                
+        //         if (grid.row === row && grid.col === col){
+                    
+        //             chosenGrid = grid;
+                
+        //         }
+        
+        //     });
+        
+        //     if(!(chosenGrid.marker === 'x'|| chosenGrid.marker === 'o')) {
+        
+                
+        //         chosenGrid.marker = marker;
+    
+        //         player1turn = player1turn === false ? true : false;
+        
+        //         consoleDisplay(gameBoard);
+        
+        //     }else{
+        //         console.log('that grid is taken!');
+        //     }
+        
+        //     gridCheck(gameBoard,this);
+    
+        //     // 'this' refers to this object
+        //     // console.log(this);
+    
+            
+        
+        // }
+
+
+        const choice = function(nodeNumber){
+    
+            console.log(name+" plays")
+    
+            
+        
+
+            let gameBoardArray = gameBoard.board;
+
+            let chosenGrid = gameBoardArray[nodeNumber];
+
+
+            // gameBoard.board.forEach(grid => {
+    
+            //     // Only add the marker to the grid that was chosen
+                
+            //     if (grid.row === row && grid.col === col){
+                    
+            //         chosenGrid = grid;
+                
+            //     }
+        
+            // });
+
+            
+        
+            if(!(chosenGrid.marker === 'x'|| chosenGrid.marker === 'o')) {
+        
+                
+                chosenGrid.marker = marker;
+    
+                player1turn = player1turn === false ? true : false;
+        
+                consoleDisplay(gameBoard);
+        
+            }else{
+                console.log('that grid is taken!');
+            }
+        
+            gridCheck(gameBoard,this);
+    
+            // 'this' refers to this object
+            // console.log(this);
+    
+            
+        
+        }
+
+
+
+
+
+    
+        return {name,choice,marker};
+    
+    };
+
+    // change this with a form input later
+    // let player1Name = prompt('Input player 1 name','') || 'Player 1';
+
+    // let player2Name = prompt('Input player 2 name','') || 'Player 2';
+
+
+
+
+
+    const player1= players('player1Name','x');
+
+
+
+
+    const player2 = players('player2Name','o');
+
+
+
+
+    
+
+    // console.log(choice1);
+
+    // player1.choice(2,0);
+
+    // player1.choice(1,1);
+
+    // player1.choice(0,2);
+
+    // player2.choice(0,0);
+
+
+
+    function gridCheck(gameBoard,player1){
+        const boardArray = gameBoard.board;
+    
+       
+        
+    
+        for (let index = 0; index < gridNumber; index++) {
+    
+            //row check
+            
+            let gridArray = [];
+    
+    
+            boardArray.forEach(grid => {
+            
+                if(grid.row === index ) {
+    
+                    gridArray.push(grid);
+    
+                }
+        
+        
+            });
+    
+            
+    
+    
+            markerCheck(gridArray,player1);
+    
+            // markerCheck(gridArray,player2);
+    
+    
+        // Column check
+            
+    
+            gridArray = [];
+    
+    
+            console.log(player1);
+            
+    
+            boardArray.forEach(grid => {
+            
+                if(grid.col === index ) {
+    
+                    gridArray.push(grid);
+    
+                }
+        
+        
+            });
+    
+    
+            markerCheck(gridArray,player1);
+    
+            // markerCheck(gridArray,player2);
+    
+            
+            // gridArray = [];
+    
+        }
+    
+    
+    
+    
+        function descendingLineCheck () {
+    
+            let gridArray = [];
+        
+            for (let j = 0; j < gridNumber; j++) {
+    
+    
+    
+                boardArray.forEach(grid => {
+    
+                    if(grid.row === j && grid.col === j){
+                        
+                        gridArray.push(grid);
+                    }
+                });
+    
+                
+                
+            }
+    
+            // console.log(gridArray);
+    
+            markerCheck(gridArray,player1);
+    
+            // markerCheck(gridArray,player2);
+    
+            gridArray =[];
+    
+        }
+    
+        descendingLineCheck();
+    
+    
+        // console.log(descendingLineArray);
+    
+    
+    
+        function ascendingLineCheck () {
+            let gridArray = [];
+    
+            // Because gridNumber is 3
+    
+            let k = gridNumber - 1
+    
+            for (let j = 0; j < gridNumber; j++) {
+    
+    
+                boardArray.forEach(grid => {
+    
+                    if(grid.row === j &&  grid.col === k){
+                        
+                        gridArray.push(grid);
+    
+                        k--;
+                    }
+                });
+    
+    
+                
+            }
+    
+    
+            // console.log(gridArray);
+    
+            // console.log(ascendingGridArray);
+    
+            // console.log(gridNumber);
+    
+            markerCheck(gridArray,player1);
+    
+            // markerCheck(gridArray,player2);
+        
+    
+        }
+    
+        ascendingLineCheck();
+    
+    
+        drawCheck(boardArray);
+
+    
+        
+    }
+    
+    
+    
+    // module
+    
+    function markerCheck(gridArrayPlaceholder,player){
+    
+        let markerCheck = [];
+    
+        gridArrayPlaceholder.forEach(grid => {
+    
+            if(grid.marker === player.marker){
+                
+                markerCheck.push(grid);
+            }
+    
+        })
+    
+        if (gridArrayPlaceholder.length === markerCheck.length) {
+    
+            console.log(player.name+' wins!');
+    
+            return;
+    
+            //create a function to disable grid input and pop up a restart button
+        
+        }
+    
+        markerCheck = [];
+    
+    
+    }
+    
+    
+    
+    
+    // module
+    
+    function drawCheck (boardArrayPlaceholder) {
+    
+        let gridArray = [];
+    
+        boardArrayPlaceholder.forEach(grid => {
+            
+            if(grid.marker === '.' || (!grid.marker)){
+    
+                gridArray.push(grid);
+    
+                
+                // return;
+            }
+            
+    
+        });
+    
+        if(gridArray.length === 0){
+    
+            console.log('Game is a draw!');
+    
+        }
+    
+    }
+
+    let p = function(row, col){
+
+        let test = player1turn? player1.choice(row,col):player2.choice(row,col);
+    
+        return test;
+    
+    }
+
+    
+    
+    // methods that are available
+    return {p};
 
 })();
 
 
 
+// console.log(gameFlow.choice1);
 
-// put this inside a module
-let player1turn = true;
-
-
+// 
 
 
 
+// this returns choices, not player object
 
 
-// Factory function 
-const players = function (name,marker){
 
-    // this works!
-    // const choose = function(row,col){
-    //     const gridLocation = 
-    //         gameBoard.board.find(grid => {
+// module for DOM
+const displayController = (function(gameBoardPlaceholder){
 
-    //         return (grid.row === row && grid.col === col);
+    const gameBoardArray = gameBoardPlaceholder.board;
 
+    const boardDOM = document.querySelector('.board');
 
-    //     });
-
-    //     gridLocation.marker = marker;
-
+    gameBoardArray.forEach(grid => {
         
-    //     console.log(gridLocation.marker);
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid');
 
-    //     consoleDisplay(gameBoard);
 
-    // };
 
-    // or use this 
+        boardDOM.appendChild(gridElement);
 
-
-    const choice = function(row,col){
-
-        console.log(name+" plays")
-
-        let chosenGrid = '';
-    
-        gameBoard.board.forEach(grid => {
-
-            // Only add the marker to the grid that was chosen
-            
-            if (grid.row === row && grid.col === col){
-                
-                chosenGrid = grid;
-            
-            }
-    
-        });
-    
-        if(!(chosenGrid.marker === 'x'|| chosenGrid.marker === 'o')) {
-    
-            
-            chosenGrid.marker = marker;
-
-            player1turn = player1turn === false ? true : false;
-    
-            consoleDisplay(gameBoard);
-    
-        }else{
-            console.log('that grid is taken!');
-        }
-    
-        gridCheck(gameBoard);
-
-        
-    
-    }
-
-    return {name,choice,marker};
-
-};
-
-
-
-
-
-
-
-const player1= players('risshi','x')
-
-// console.log(player1);
-
-const player2 = players('ganesh','o');
-
-// console.log(player2);
-
-
-player1.choice(2,0);
-
-
-
-
-
-
-
-
-
-let p = function(row, col){
-
-    let test = player1turn? player1.choice(row,col):player2.choice(row,col);
-
-    return test;
-
-}
-
-
-
-// module
-
-function gridCheck(gameBoard){
-    const boardArray = gameBoard.board;
-
-    
-
-    for (let index = 0; index < gridNumber; index++) {
-
-
-
-
-        //row check
-        
-        let gridArray = [];
-
-
-        boardArray.forEach(grid => {
-        
-            if(grid.row === index ) {
-
-                gridArray.push(grid);
-
-            }
-    
-    
-        });
-
-        
-
-
-        markerCheck(gridArray,player1);
-
-        markerCheck(gridArray,player2);
-
-
-    // Column check
-        
-
-        gridArray = [];
-
-
-        
-
-        boardArray.forEach(grid => {
-        
-            if(grid.col === index ) {
-
-                gridArray.push(grid);
-
-            }
-    
-    
-        });
-
-
-        markerCheck(gridArray,player1);
-
-        markerCheck(gridArray,player2);
-
-        
-        // gridArray = [];
-
-    }
-
-
-
-
-    function descendingLineCheck () {
-
-        let gridArray = [];
-    
-        for (let j = 0; j < gridNumber; j++) {
-
-
-
-            boardArray.forEach(grid => {
-
-                if(grid.row === j && grid.col === j){
-                    
-                    gridArray.push(grid);
-                }
-            });
-
-            
-            
-        }
-
-        // console.log(gridArray);
-
-        markerCheck(gridArray,player1);
-
-        markerCheck(gridArray,player2);
-
-        gridArray =[];
-
-    }
-
-    descendingLineCheck();
-
-
-    // console.log(descendingLineArray);
-
-
-
-    function ascendingLineCheck () {
-
-        let gridArray = [];
-
-        // Because gridNumber is 3
-
-        let k = gridNumber - 1
-
-        for (let j = 0; j < gridNumber; j++) {
-
-
-            boardArray.forEach(grid => {
-
-                if(grid.row === j &&  grid.col === k){
-                    
-                    gridArray.push(grid);
-
-                    k--;
-                }
-            });
-
-
-            
-        }
-
-
-        // console.log(gridArray);
-
-        // console.log(ascendingGridArray);
-
-        // console.log(gridNumber);
-
-        markerCheck(gridArray,player1);
-
-        markerCheck(gridArray,player2);
-    
-
-    }
-
-    ascendingLineCheck();
-
-
-    drawCheck(boardArray);
-
-    
-}
-
-
-
-// module
-
-function markerCheck(gridArrayPlaceholder,player){
-
-    let markerCheck = [];
-
-    gridArrayPlaceholder.forEach(grid => {
-
-        if(grid.marker === player.marker){
-            
-            markerCheck.push(grid);
-        }
-
-    })
-
-    if (gridArrayPlaceholder.length === markerCheck.length) {
-
-        console.log(player.name+' wins!');
-
-        return;
-
-        //create a function to disable grid input and pop up a restart button
-    
-    }
-
-    markerCheck = [];
-
-
-}
-
-
-
-
-// module
-
-function drawCheck (boardArrayPlaceholder) {
-
-    let gridArray = [];
-
-    boardArrayPlaceholder.forEach(grid => {
-        
-        if(grid.marker === '.' || (!grid.marker)){
-
-            gridArray.push(grid);
-
-            
-            // return;
-        }
-        
 
     });
 
-    if(gridArray.length === 0){
 
-        console.log('Game is a draw!');
+    const gridDOM = document.querySelectorAll('.grid');
+
+
+    // To add data attribute
+    for (let k = 0; k < gridDOM.length; k++) {
+
+        gridDOM[k].setAttribute('data-grid',k);
 
     }
 
-}
+    
+
+
+    gridDOM.forEach(grid => {
+        
+        grid.addEventListener('click', function(){
+
+            const gameBoardArray = gameBoard.board;
+            
+            const clickedGrid = grid.dataset.grid;
+
+            gameFlow.p(clickedGrid);
+
+            grid.textContent = gameBoardArray[clickedGrid].marker;
+
+            console.log(clickedGrid);
+
+        })
+    });
+
+
+})(gameBoard);
